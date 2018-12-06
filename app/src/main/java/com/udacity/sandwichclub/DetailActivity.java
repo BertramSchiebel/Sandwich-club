d640283d19db7b0db83342f24bb052c50043da46
Bertram <bjs70018@gmail.com>
2018-12-07 00:23:14 +0100

    show all details in Detailview

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -43,7 +44,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +57,27 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich)
+    {
+        TextView akaNamesTexView = findViewById(R.id.also_known_tv);
+        TextView ingredientsTextView = findViewById(R.id.ingredients_tv);
+        TextView originTextView = findViewById(R.id.origin_tv);
+        TextView descriptionTextView = findViewById(R.id.description_tv);
+
+        akaNamesTexView.setText("");
+        for (String akaName:sandwich.getAlsoKnownAs())
+        {
+            akaNamesTexView.append(akaName+"\n");
+        }
+
+        ingredientsTextView.setText("");
+        for(String ingridient:sandwich.getIngredients())
+        {
+            ingredientsTextView.append(ingridient +"\n");
+        }
+
+        originTextView.setText(sandwich.getPlaceOfOrigin());
+        descriptionTextView.setText(sandwich.getDescription());
 
     }
 }
